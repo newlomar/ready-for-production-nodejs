90bf75e03db5f0ec745550645a15693316bffd18
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-08-04 08:53:44 -0300

    running server

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,14 @@
-const sum = (a: number, b: number) => a + b;
-setInterval(() => console.log(sum(2, 3)), 1000);
+import http from "http";
+
+import express from "express";
+
+const router = express();
+
+const { PORT = 3000 } = process.env;
+
+const server = http.createServer(router);
+
+server.listen(PORT, () => {
+  //setInterval(() => console.log(`Server is running on http://localhost:${PORT}`), 1000);
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
