3b75b1317e83c3c63bd2ab8d601069ff5e07ca50
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-08-04 09:01:20 -0300

    addin middlewares

---
+++ src/middleware/common.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import cors from "cors";
+import parser from "body-parser";
+import compression from "compression";
+import { textChangeRangeIsUnchanged } from "typescript";
+
+export const handleCors = (router: Router) => router.use(cors({ credentials: true, origin: true })) // TODO: testar sem retornar para ver o que acontece
+
+export const handleBodyRequestParsing = (router: Router) => {
+  router.use(parser.urlencoded({ extended: true }));
+  router.use(parser.json());
+}
+
+export const handleCompression = (router: Router) => {
+  router.use(compression());
+}
