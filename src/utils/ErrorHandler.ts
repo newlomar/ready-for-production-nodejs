a7f0340f236c8a700165613f892ad381a3571254
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-08-07 06:51:57 -0300

    adding error handlers utils

---
+++ src/utils/ErrorHandler.ts
@@ -0,0 +1,8 @@
+import type { Response, NextFunction} from "express";
+//import { HTTPClientError, HTTP404Error } from "../utils/httpErrors.js"/
+
+export const notFoundError = () => {
+  // throw new HTTP404Error("Methood not found.");
+}
+
+export const clientError = (err: Error, res: Response, next: NextFunction) => {}
