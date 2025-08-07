4558cb3b92727162acb08f40c4bb45a9ab6f3a41
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-08-07 06:46:57 -0300

    adding error handlers

---
+++ src/middleware/errorHandlers.ts
@@ -0,0 +1,22 @@
+import {  Router } from "express";
+import type { Request, Response, NextFunction } from "express";
+//import * as ErrorHandler from "../utils/ErrorHandler.js";
+
+const handle404Error = (router: Router) => {
+  router.use((req: Request, res: Response) => {
+    
+  }) 
+}
+
+const handleClientError = (router: Router) => {
+  router.use((req: Request, res: Response, next: NextFunction) => {
+    
+  }) 
+}
+const handleServerError = (router: Router) => {
+  router.use((req: Request, res: Response, next: NextFunction) => {
+    
+  }) 
+}
+
+export default [handle404Error, handleClientError, handleServerError];
