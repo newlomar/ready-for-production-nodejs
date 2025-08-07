3812871344d9b4f713ebeafc623e188ef4c650d5
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-08-07 07:14:47 -0300

    adding error handlers utils

--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -3,12 +3,24 @@ import type { Response, NextFunction} from "express";
 
 export const notFoundError = () => {
   // throw new HTTP404Error("Methood not found.");
+  throw new Error('Not Found');
 }
 
 export const clientError = (err: Error, res: Response, next: NextFunction) => {
-  
+  if (err) {
+    console.warn(err);
+    res.status(400).send(err.message)
+  } else {
+    next(err);
+  }
 }
 
 export const serverError = (err: Error, res: Response, next: NextFunction) => {
-  
+  console.error(err);
+
+  if (process.env.NODE_ENV === "production") {
+    res.status(500).send("Internal Server Error");
+  } else {
+    res.status(500).send(err.stack);
+  }
 }
