f0e4df8645b41f884a5454a80abdc8ec62be8622
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-08-10 10:46:13 -0300

    fixing response

--- src/services/search/providers/OpenCageDataProvider.ts
+++ src/services/search/providers/OpenCageDataProvider.ts
@@ -4,8 +4,8 @@ dotenv.config();
 
 export const getPlaces = async (query: string) => {
   const key = process.env.OPEN_CAGE_DATA_KEY;
-  const url = `https://api.opencagedata.com/geocode/v1/json?q=${query}key=${key}`
+  const url = `https://api.opencagedata.com/geocode/v1/json?q=${query}&&key=${key}`
   const response = await (await fetch(url)).json();
-
-  return JSON.parse(response);
+  console.log(JSON.stringify(response))
+  return response;
 }
