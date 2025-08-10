61b926f85c2f087a7a38c3e96e5f166c3a6983ec
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-08-10 10:30:14 -0300

    new root message return

--- src/services/search/routes.ts
+++ src/services/search/routes.ts
@@ -1,11 +1,12 @@
 import type { Request, Response } from "express";
+//import { getPlace }
 
 export default [
   {
     path: "/",
     method: "get",
     handler: async (req: Request, res: Response) => {
-      res.send("Hello world!");
+      res.send("<h1>Hello, world!</h1><h2>Brazil, Morocco, London to Ibiza</h2><h2>Straight to LA, New York, Vegas to Africa!</h2>");
     }
   }
 ]
