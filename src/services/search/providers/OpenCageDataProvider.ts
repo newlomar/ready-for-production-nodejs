520f3b7b83de887ed70e43dd13ff1f0531bb58fd
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-08-10 10:46:32 -0300

    removing log

--- src/services/search/providers/OpenCageDataProvider.ts
+++ src/services/search/providers/OpenCageDataProvider.ts
@@ -6,6 +6,5 @@ export const getPlaces = async (query: string) => {
   const key = process.env.OPEN_CAGE_DATA_KEY;
   const url = `https://api.opencagedata.com/geocode/v1/json?q=${query}&&key=${key}`
   const response = await (await fetch(url)).json();
-  console.log(JSON.stringify(response))
   return response;
 }
