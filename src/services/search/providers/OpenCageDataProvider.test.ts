304cfa9986ee78fbd9d11914cbdfb81d467ff515
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-08-13 08:48:42 -0300

    trying to understand and fix jest running errors

---
+++ src/services/search/providers/OpenCageDataProvider.test.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { getPlaces } from "./OpenCageDataProvider.js";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('OpenCageDataProvider', () => {
+  test('an empty query string', async () => {
+    mockedAxios.get.mockResolvedValue({ data: { features: [] } });
+    const result = await getPlaces('Rio de Janeiro');
+    expect(result).toEqual({ features: [] })
+  });
+
+  test('an invalid non-json response', () => {
+    mockedAxios.get.mockRejectedValue(new Error('Service Unavailable.'));
+    await expect(getPlaces('Chamonix')).rejects.toThrow('Service Unavailable');
+  })
+})
