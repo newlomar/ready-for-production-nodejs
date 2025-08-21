b0de0f3b3fe2d2619455def702332f7d1791e31e
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-08-21 07:56:33 -0300

    adding prettier

--- src/services/search/routes.test.ts
+++ src/services/search/routes.test.ts
@@ -1,12 +1,16 @@
-import express from "express";
-import type { Router } from "express";
-import request from "supertest";
-import { applyMiddleware, applyRoutes } from "../../utils/index.ts";
-import axios from "axios";
-import middleware from "../../middleware/index.ts";
-import errorHandlers from "../../middleware/errorHandlers.ts";
-import routes from "./routes.ts";
+import express from 'express';
+import type { Router } from 'express';
+import request from 'supertest';
+import { applyMiddleware, applyRoutes } from '../../utils/index.ts';
+import axios from 'axios';
+import middleware from '../../middleware/index.ts';
+import errorHandlers from '../../middleware/errorHandlers.ts';
+import routes from './routes.ts';
 
-jest.mock("axios");
+jest.mock('axios');
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+mockedAxios.get.mockResolvedValue({ data: { features: [] } });
+
+describe('routes', () => {});
