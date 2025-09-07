a69df3d61f66621f207c77eea08519a7d97f27c9
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-09-07 12:38:46 -0300

    removing axios

--- src/services/search/routes.test.ts
+++ src/services/search/routes.test.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import type { Router } from 'express';
 import request from 'supertest';
 import { applyMiddleware, applyRoutes } from '../../utils/index.ts';
-import axios from 'axios';
 import middleware from '../../middleware/index.ts';
 import errorHandlers from '../../middleware/errorHandlers.ts';
 import routes from './routes.ts';
